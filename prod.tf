959bc7d205579d4bc413c422beaf19d777e900e5
riosnatalia <natalia.rios@wexinc.com>
2022-06-29 10:45:24 -0300

    Adopt default vpc and rename tf file to prod

--- prod.tf
+++ prod.tf
@@ -2,7 +2,10 @@ provider "aws" {
   profile = "default"
   region = "us-west-2"
  }
-resource "aws_s3_bucket" "tf_course" {
+
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-natalia-2022-06-27"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
