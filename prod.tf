3236ad170ad67dc72e639f572a56293635d94e7d
riosnatalia <natalia.rios@wexinc.com>
2022-07-01 10:35:55 -0300

    Web instances and elastic ip

--- prod.tf
+++ prod.tf
@@ -36,3 +36,24 @@ resource "aws_security_group" "prod_web" {
     "Terraform" : "true"
   }
 }
+
+resource "aws_instance" "prod_web" {
+  ami           = "ami-0fded6ad5b3f77f8d"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}    
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
